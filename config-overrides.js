608e70e2e3d81e755bba343c96ab8eacfed20e72
viewweiwu <648764190@qq.com>
2020-05-05 19:36:14 +0800

    修改配置

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,11 @@ const { override, addLessLoader, addWebpackAlias } = require('customize-cra');
 const path = require('path')
 
 module.exports = {
+  paths: function (paths) {
+    paths.publicUrlOrPath = './'
+    paths.appBuild = path.resolve(__dirname, 'dist')
+    return paths
+  },
   webpack: override(
     addWebpackAlias({
       '@': path.resolve(__dirname, 'src'),
